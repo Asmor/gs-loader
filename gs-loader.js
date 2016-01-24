151a36bd17cfb6bca13ba806bd0f0b778cd0203b
asmor <itoltz@gmail.com>
2016-01-24 02:24:07 -0500

    Loads correct header names

--- gs-loader.js
+++ gs-loader.js
@@ -46,10 +46,41 @@ var loadGS = (function () {
 		});
 	}
 
+	// The lists feed changes header names by removing all characters besides dashes, letters, and
+	// numbers, and then making them lowercase. Then if there's a collision it tacks on _2, _3, etc.
+	function getRealHeaderNames(data) {
+		var cols = [];
+		data.forEach(function (cell) {
+			var cellInfo = cell.gs$cell;
+
+			if ( cellInfo.row === "1" ) {
+				// Columns are 1-indexed, not 0-indexed
+				cols[cellInfo.col - 1] = cellInfo.$t;
+			}
+		});
+
+		var colNames = {};
+		cols.forEach(function (col) {
+			var normalized = col.replace(/[^-a-z0-9]/ig, "").toLowerCase();
+			var proposedName = normalized;
+			var suffix = 2;
+
+			// If we've got a collision, add a numeric suffix
+			while ( colNames[proposedName] ) {
+				proposedName = normalized + "_" + suffix++;
+			}
+
+			colNames[proposedName] = col;
+		});
+
+		return colNames;
+	}
+
 	function getWorksheets(id) {
 		var url = "https://spreadsheets.google.com/feeds/worksheets/" + id + "/public/full";
 		var worksheetPromises = [];
 		var worksheets = {};
+		var realHeaders = {};
 
 		// Step 1: Get a list of all the worksheets in the spreadsheet
 		return getSheetsJsonp(url).then(function (data) {
@@ -60,13 +91,32 @@ var loadGS = (function () {
 				// Step 2: For each worksheet, parse its listfeed
 				worksheet.link.some(function (link) {
 					if ( link.rel.match(/listfeed/) ) {
-						worksheetPromises.push(getSheetsJsonp(link.href).then(parseLine.bind(null, ws)));
-						return true;
+						worksheetPromises.push(
+							getSheetsJsonp(link.href).then(parseLine.bind(null, ws))
+						);
+					} else if ( link.rel.match(/cellsfeed/) ) {
+						worksheetPromises.push(
+							getSheetsJsonp(link.href).then(function (data) {
+								realHeaders[name] = getRealHeaderNames(data);
+							})
+						);
 					}
 				});
 			});
 
 			return Promise.all(worksheetPromises).then(function () {
+				// Translate the normalized listfeed column names to the real names
+				Object.keys(worksheets).forEach(function (name) {
+					var ws = worksheets[name];
+					Object.keys(realHeaders[name]).forEach(function (normalizedColName) {
+						var realName = realHeaders[name][normalizedColName];
+
+						if ( realName === normalizedColName ) { return; }
+
+						ws[realName] = ws[normalizedColName];
+						delete ws[normalizedColName];
+					});
+				});
 				return worksheets;
 			});
 		});
